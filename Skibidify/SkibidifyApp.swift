fec1e847d3049a0152f3f3cdbdcad87f223f083c
Jia Chen <github@jiachen.app>
2024-10-30 14:46:41 +0800

    Initial Commit

---
+++ Skibidify/SkibidifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkibidifyApp.swift
+//  Skibidify
+//
+//  Created by Jia Chen Yee on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SkibidifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
